a35383ef96aa1d0191abcc092c9d1781f9bd08e7
thobela <thobela.sixpence@gmail.com>
2025-01-12 20:26:07 +0200

    updated the name of the project from the setup script

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ml-zoomcamp-course-capstone-project",
+    version="0.0.1",
+    author="Thobela Sixpence",
+    author_email='thobela.sixpence@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
